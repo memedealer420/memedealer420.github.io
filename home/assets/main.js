2b6b24cec98dbf89c423949026e4f5b58dceb872
ExtortTheSystem <43294106+ExtortTheSystem@users.noreply.github.com>
2019-04-05 22:34:33 +1100

    big commit
    finished part 1 of nav bar. need to figure out how to add elements with atributes. Clone of 2048 added

--- home/assets/main.js
+++ home/assets/main.js
@@ -56,13 +56,72 @@ $(document).ready(function() {
       })
     })
 
-//Restructured Navigation Bar
+//Restructured Navigation Bar Temporary Fix
 $(document).ready(function(){
-document.getElementById("li1").href = "https://memedealer420.github.io/home";
+document.getElementById("li1").href = "index.html";
 document.getElementById("li1").innerHTML = "MemeDealer Yeet";
-document.getElementById("li2").href= "bubbles.html";
-document.getElementById("li2").innerHTML = "Bubble Shooter";
+
+document.getElementById("li2").href= "2048.html";
+document.getElementById("li2").innerHTML = "2048";
+
+document.getElementById("li3").href= "bubbles.html";
+document.getElementById("li3").innerHTML = "Bubble Shooter";
+
+document.getElementById("li4").href= "cat-mario.html";
+document.getElementById("li4").innerHTML = "Cat Mario";
+
+document.getElementById("li5").href= "fireboyandwatergirl.html";
+document.getElementById("li5").innerHTML = "Fire Boy and Water Girl";
+
+document.getElementById("li6").href= "fireboywatergirl2.html";
+document.getElementById("li6").innerHTML = "Fireboy and Watergirl 2";
+
+document.getElementById("li7").href= "sadwheels.html";
+document.getElementById("li7").innerHTML = "Happy Wheels";
+
+document.getElementById("li8").href= "NotNGame2.html";
+document.getElementById("li8").innerHTML = "N-Game 2";
+
+document.getElementById("li9").href= "possible_quiz.html";
+document.getElementById("li9").innerHTML = "Possible Quiz";
+
+document.getElementById("li10").href= "snek.html";
+document.getElementById("li10").innerHTML = "Snake";
+
+document.getElementById("li11").href= "dora.html";
+document.getElementById("li11").innerHTML = "Spot the Numbers";
+
+document.getElementById("li12").href= "basketballheads.html";
+document.getElementById("li12").innerHTML = "Sports Basketball Heads";
+
+document.getElementById("li13").href= "tetris.html";
+document.getElementById("li13").innerHTML = "Tetris";
+
+document.getElementById("li14").href= "#";
+document.getElementById("li14").innerHTML = "";
+
+document.getElementById("li15").href= "#";
+document.getElementById("li15").innerHTML = "";
+
+document.getElementById("li16").href= "#";
+document.getElementById("li16").innerHTML = "";
+
+document.getElementById("li17").href= "#";
+document.getElementById("li17").innerHTML = "";
+
+document.getElementById("li18").href= "#";
+document.getElementById("li18").innerHTML = "";
+
+document.getElementById("li19").href= "#";
+document.getElementById("li19").innerHTML = "";
+
+document.getElementById("li20").href= "#";
+document.getElementById("li20").innerHTML = "";
 //Create New Value for li1
+
+//Need Help with this one
+
+/*
 var li3p = document.createElement("li");
 var li3n = document.createTextNode("Yeet");
 li3p.appendChild(li3n);
@@ -76,6 +135,6 @@ var element = document.getElementById("nav");
 var child = document.getElementById("li3");
 element.appendChild(li3);
 
-
+*/
 
 })
